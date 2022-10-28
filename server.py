044852d92cbfa8f3b453c3d112eeb1868ba52403
Sean Oplinger <solekibbles4@gmail.com>
2022-10-27 22:57:38 -0400

    add code to server.py and create dummyclient.py

--- server.py
+++ server.py
@@ -12,9 +12,17 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 UDPServerSocket.bind((localIP, localPort))
 
-print ("UDP Server Listening")
+print("UDP Server Listening")
 
-while(True):
+while (True):
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    UDPServerSocket.sendto(bytesToSend, address)
