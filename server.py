cb86e59cbbe22aef03ec97c6978921250dfa21b6
Sean Oplinger <solekibbles4@gmail.com>
2022-11-16 21:36:30 -0500

    add userHandler

--- server.py
+++ server.py
@@ -4,22 +4,9 @@ import random
 import sys
 import json
 
-with open("dict.txt", "r") as file:
-    allText = file.read()
-    words = list(map(str, allText.split()))
 
-localIP = "127.0.0.1"
-localPort = 20001
-bufferSize = 1024
+def userHandler(clientInfo):
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-UDPServerSocket.bind((localIP, localPort))
-
-print("UDP Server Listening")
-
-while (True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
 
@@ -36,3 +23,23 @@ while (True):
     bytesToSend = str.encode(random.choice(words))
 
     UDPServerSocket.sendto(bytesToSend, address)
+
+
+with open("dict.txt", "r") as file:
+    allText = file.read()
+    words = list(map(str, allText.split()))
+
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+UDPServerSocket.bind((localIP, localPort))
+
+print("UDP Server Listening")
+
+while (True):
+
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    userHandler(bytesAddressPair)
